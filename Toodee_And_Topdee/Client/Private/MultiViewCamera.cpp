8e45c91d2161b8285a421bc987a9541918cee145
yetak <yeji4903@naver.com>
2025-05-22 16:31:18 +0900

    feat : Top-Quter 카메라 구현

---
+++ Toodee_And_Topdee/Client/Private/MultiViewCamera.cpp
@@ -0,0 +1,145 @@
+#include "MultiViewCamera.h"
+#include "GameInstance.h"
+
+
+CMultiViewCamera::CMultiViewCamera(LPDIRECT3DDEVICE9 pGraphic_Device)
+    : CGameObject{ pGraphic_Device }
+{
+}
+
+CMultiViewCamera::CMultiViewCamera(const CMultiViewCamera& Prototype)
+    : CGameObject{ Prototype }
+{
+}
+
+HRESULT CMultiViewCamera::Initialize_Prototype()
+{
+    return S_OK;
+}
+
+HRESULT CMultiViewCamera::Initialize(void* pArg)
+{
+    if (FAILED(Ready_Components(pArg)))
+        return E_FAIL;
+
+    CAMERA_DESC* pDesc = static_cast<CAMERA_DESC*>(pArg);
+
+    m_pTransformCom->Set_State(STATE::POSITION, pDesc->vEye);
+    m_pTransformCom->Look_At(pDesc->vAt);
+
+    m_OffsetLength = pDesc->vEye.y;
+    m_fFovy = pDesc->fFovy;
+    m_fAspect = static_cast<_float>(g_iWinSizeX) / g_iWinSizeY;
+    m_fNear = pDesc->fNear;
+    m_fFar = pDesc->fFar;
+    return S_OK;
+}
+
+void CMultiViewCamera::Priority_Update(_float fTimeDelta)
+{
+    _bool newkey = GetKeyState('X') & 0x8000;
+
+    if (!m_bOldKey && newkey)
+        m_bRotating = true;
+
+    m_bOldKey = newkey;
+
+    if (m_bRotating == true)
+        ChangeView(fTimeDelta);
+
+    m_ProjMatrix = *(D3DXMatrixPerspectiveFovLH(&m_ProjMatrix, m_fFovy, m_fAspect, m_fNear, m_fFar));
+    m_pGraphic_Device->SetTransform(D3DTS_VIEW, m_pTransformCom->Get_WorldMatrix_Inverse());
+    m_pGraphic_Device->SetTransform(D3DTS_PROJECTION, &m_ProjMatrix);
+}
+
+void CMultiViewCamera::Update(_float fTimeDelta)
+{
+}
+
+void CMultiViewCamera::Late_Update(_float fTimeDelta)
+{
+}
+
+HRESULT CMultiViewCamera::Render()
+{
+    return S_OK;
+}
+
+void CMultiViewCamera::ChangeView(_float fTimeDelta)
+{
+    // [카메라 속도 기반 세팅]
+    _float fDelta = m_ChangeSpeed * fTimeDelta;             //Angle 움직임 속도 제어
+    _float fPosDelta = m_ChangeSpeed * fTimeDelta * 10;     //Position 움직임 속도 제어
+    m_fCurrentAngle += fDelta;
+
+    // [카메라 회전->이동->zoom 적용]
+    if (m_bType == CAM_TYPE::TOP)
+    {
+        m_pTransformCom->Turn(m_pTransformCom->Get_State(STATE::RIGHT), -D3DXToRadian(fDelta));
+        m_fCurrentPos -= fPosDelta;
+        m_pTransformCom->Set_State(STATE::POSITION, _float3(0.f, m_OffsetLength, m_fCurrentPos));
+        m_pTransformCom->Go_Straight(-fTimeDelta * 15.f);
+    }
+    else
+    {
+        m_pTransformCom->Turn(m_pTransformCom->Get_State(STATE::RIGHT), D3DXToRadian(fDelta));
+        m_fCurrentPos += fPosDelta;
+        m_pTransformCom->Set_State(STATE::POSITION, _float3(0.f, m_OffsetLength, m_fCurrentPos));
+        m_pTransformCom->Go_Straight(fTimeDelta * 15.f);
+    }
+
+    _float3 tmp = m_pTransformCom->Get_State(STATE::POSITION);
+    // [목표 각도 달성하면 Flag 해제]
+    if (m_fCurrentAngle >= m_fTargetAngle)
+    {
+        m_fCurrentAngle = 0;
+        m_bRotating = false;
+        if (m_bType == CAM_TYPE::TOP)
+            m_bType = CAM_TYPE::QURTER;
+        else
+            m_bType = CAM_TYPE::TOP;
+    }
+}
+
+HRESULT CMultiViewCamera::Ready_Components(void* pArg)
+{
+    /* For.Com_Transform */
+    if (FAILED(__super::Add_Component(ENUM_CLASS(LEVEL::LEVEL_STATIC), TEXT("Prototype_Component_Transform"),
+        TEXT("Com_Transform"), reinterpret_cast<CComponent**>(&m_pTransformCom), pArg)))
+        return E_FAIL;
+
+    return S_OK;
+}
+
+CMultiViewCamera* CMultiViewCamera::Create(LPDIRECT3DDEVICE9 pGraphic_Device)
+{
+    CMultiViewCamera* pInstance = new CMultiViewCamera(pGraphic_Device);
+
+    if (FAILED(pInstance->Initialize_Prototype()))
+    {
+        MSG_BOX(TEXT("Failed to Created : CMultiViewCamera"));
+        Safe_Release(pInstance);
+    }
+
+    return pInstance;
+}
+
+CGameObject* CMultiViewCamera::Clone(void* pArg)
+{
+    CMultiViewCamera* pInstance = new CMultiViewCamera(*this);
+
+    if (FAILED(pInstance->Initialize(pArg)))
+    {
+        MSG_BOX(TEXT("Failed to Cloned : CMultiViewCamera"));
+        Safe_Release(pInstance);
+    }
+
+    return pInstance;
+}
+
+void CMultiViewCamera::Free()
+{
+    __super::Free();
+
+    Safe_Release(m_pTransformCom);
+}
---
+++ Toodee_And_Topdee/Client/Public/MultiViewCamera.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "GameObject.h"
+#include "Transform.h"
+
+BEGIN(Client)
+
+/*
+	[Level에서 세팅할 CAMERA_DESC 구조체 값]
+ 
+	CameraDesc.vEye = _float3(0.f, 250.f, 0.f);
+	CameraDesc.vAt = _float3(0.f, 0.f, 1.f );
+
+	CameraDesc.fFovy = D3DXToRadian(60.0f);
+	CameraDesc.fNear = 0.1f;
+	CameraDesc.fFar = 1000.f;
+	CameraDesc.fSpeedPerSec = 10.f;
+	CameraDesc.fRotationPerSec = D3DXToRadian(90.0f);
+	CameraDesc.fMouseSensor = 0.3f;
+*/
+
+class CMultiViewCamera final : public CGameObject
+{
+public:
+	enum class CAM_TYPE { TOP, QURTER };
+	typedef struct tagCameraDesc : CTransform::TRANSFORM_DESC
+	{
+		_float3						vEye{}, vAt{};
+		_float						fFovy{}, fNear{}, fFar{};
+		_float						fMouseSensor{};
+	}CAMERA_DESC;
+
+private:
+	CMultiViewCamera(LPDIRECT3DDEVICE9 pGraphic_Device);
+	CMultiViewCamera(const CMultiViewCamera& Prototype);
+	virtual ~CMultiViewCamera() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void Priority_Update(_float fTimeDelta) override;
+	virtual void Update(_float fTimeDelta) override;
+	virtual void Late_Update(_float fTimeDelta) override;
+	virtual HRESULT Render() override;
+
+	_float4x4	Get_ProjMatrix() { return m_ProjMatrix; };
+	void		ChangeView(_float fTimeDelta);
+
+	HANDLE							m_hThread = {};
+
+private:
+	CTransform* m_pTransformCom = { nullptr };
+	_float4x4					m_ProjMatrix{};
+	_float						m_fFovy{}, m_fAspect{}, m_fNear{}, m_fFar{};
+	CRITICAL_SECTION			m_CriticalSection = {};
+
+private:
+	POINT						m_OldPoint = {};
+	_bool						m_bOldKey = {};
+	_bool						m_bRotating = false;
+	_float						m_fTargetAngle = 10.f;
+	_float						m_fCurrentAngle = {};
+	_float						m_fCurrentPos = {};
+	_float						m_OffsetLength;
+	_float						m_ChangeSpeed = 40.f;
+	CAM_TYPE					m_bType = CAM_TYPE::TOP;
+
+private:
+	HRESULT Ready_Components(void* pArg);
+
+
+public:
+	static CMultiViewCamera* Create(LPDIRECT3DDEVICE9 pGraphic_Device);
+	virtual CGameObject* Clone(void* pArg) override;
+	virtual void Free() override;
+
+
+};
+
+END
