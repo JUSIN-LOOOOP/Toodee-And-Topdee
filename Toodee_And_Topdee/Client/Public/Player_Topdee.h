158ceac06c5508e369463bf0d3f43b2032791290
Kimjunghun <myhoon1015@gmail.com>
2025-05-25 16:18:04 +0900

    refactor : Player_Topdee 코드 수정
    FSM 구조로 수정

--- Toodee_And_Topdee/Client/Public/Player_Topdee.h
+++ Toodee_And_Topdee/Client/Public/Player_Topdee.h
@@ -7,9 +7,8 @@ class CPlayer_Topdee final : public CPlayer
 {
 private:
 	/* 순서대로 리소스 번호 찾아감 (아래, 횡, 아래 대각선, 위 대각선, 위) */
-	enum MOVE_DIRECTION { MD_DOWN, MD_TRANSVERSE, MD_DIAGONAL_DOWN, MD_DIAGONAL_UP, MD_UP };
 	/* 비트 연산용 input enum class */
-	enum INPUT_DIRECTION { NONE =0, ID_UP = 1 <<0, ID_DOWN = 1 << 1, ID_LEFT = 1 << 2, ID_RIGHT = 1 << 3};
+
 
 private:
 	CPlayer_Topdee(LPDIRECT3DDEVICE9 pGraphic_Device);
@@ -24,15 +23,11 @@ public:
 	virtual void Late_Update(_float fTimeDelta) override;
 	virtual HRESULT Render() override;
 
-private:
-	/*비트연산으로 방향 결정 */
-	MOVE_DIRECTION Check_Move_Direction(_uint iInputState);
+	virtual void Move(_float fTimeDelta) override;
+	virtual void Action() override;
 
 private:
-	/* 플레이어가 보는 방향 */
-	MOVE_DIRECTION m_eMoveDir = {};
-	_uint m_iInputState = {};
-	_uint m_iOldInputState = {};
+	MOVEDIRECTION m_eCurrentMoveDir = {};
 	/* Action 상자 들기 */
 	_bool		m_bIsAttach = { false };
 	
@@ -40,10 +35,15 @@ private:
 	_bool m_bInput = {};
 	
 private:
+	_uint KeyInput();
+	void Change_MoveDir(_uint iInputData);
+	MOVEDIRECTION ComputeMoveDirection(_uint iInputData);
+
 	HRESULT Ready_Components();
+	HRESULT Ready_States();
 	HRESULT Begin_RenderState();
 	HRESULT End_RenderState();
-
+	
 public:
 	static CPlayer_Topdee* Create(LPDIRECT3DDEVICE9 pGraphic_Device);
 	virtual CGameObject* Clone(void* pArg) override;
