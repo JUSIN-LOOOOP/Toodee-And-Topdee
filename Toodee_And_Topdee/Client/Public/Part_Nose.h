e0e77e20a76d00343fee39dbaa3dfb1d91a6fef1
AtoTony <syd9227@naver.com>
2025-05-29 20:05:50 +0900

    Feature/monster_pig
    * init  : 다시 세팅
    
    * feat : 텍스쳐 추가
    
    * feat : 파츠 컴포넌트(부모클래스) 추가
    
    * feat : Pig몬스터 및 파츠 추가
    
    * chore : 필터 정리

---
+++ Toodee_And_Topdee/Client/Public/Part_Nose.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "Parts.h"
+
+BEGIN(Engine)
+class CVIBuffer_Rect;
+class CTexture;
+class CTransform;
+END
+
+BEGIN(Client)
+
+class CPart_Nose final : public Engine::CParts
+{
+private:
+	CPart_Nose(LPDIRECT3DDEVICE9 pGraphic_Device);
+	CPart_Nose(const CPart_Nose& Prototype);
+	virtual ~CPart_Nose() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void	Update(CTransform* pTransform, _float fTimeDelta, _float InputWidth, _float InputDepth) override;
+	virtual HRESULT Render(void* pArg) override;
+
+private:
+	_int   m_iTextureIndex = {};
+	_float m_fWidth = {};
+	_float m_fDepth = {};
+
+public:
+	static CPart_Nose* Create(LPDIRECT3DDEVICE9 pGraphic_Device);
+	virtual CComponent* Clone(void* pArg) override;
+	virtual void Free() override;
+};
+
+
+END
