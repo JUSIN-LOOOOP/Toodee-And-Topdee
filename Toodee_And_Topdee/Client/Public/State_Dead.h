e79a5582da189e947d888f75b3d834ec89195843
Kimjunghun <myhoon1015@gmail.com>
2025-06-03 22:06:53 +0900

    feat : spikes, State_dead

---
+++ Toodee_And_Topdee/Client/Public/State_Dead.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "PlayerState.h"
+
+BEGIN(Client)
+
+class CState_Dead final : public CPlayerState
+{
+private:
+	CState_Dead();
+	virtual ~CState_Dead() = default;
+
+
+public:
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void Enter(CPlayer* pPlayer) override;
+	virtual void HandleInput(CPlayer* pPlayer, _uint iInputData, _float fTimeDelta) override;
+	virtual void Update(CPlayer* pPlayer, _float fTimeDelta) override;
+	virtual void Exit(CPlayer* pPlayer) override;
+	virtual void UpdateAnim(_float fTimeDelta) override;
+
+public:
+	static CState_Dead* Create(void* pArg);
+	virtual void Free();
+};
+
+END
