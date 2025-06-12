3d01695db4f85da87d1214245ff165ef61ee78cc
yetak <yeji4903@naver.com>
2025-06-12 13:10:53 +0900

    resources : 이펙트 리소스 추가

--- Toodee_And_Topdee/Copy.bat
+++ Toodee_And_Topdee/Copy.bat
@@ -3,6 +3,7 @@
 xcopy			/y			.\Engine\Public\*.*			.\EngineSDK\Inc\	
 
 xcopy			/y			.\Engine\Bin\Engine.dll		.\Client\Bin\
+xcopy			/y			.\Engine\Bin\Engine.pdb		.\Client\Bin\
 
 xcopy			/y			.\Engine\Bin\Engine.lib		.\EngineSDK\Lib\
 
